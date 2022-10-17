8194532db0a531f31ce2f7de193ab02737f65b2a
zeenu <a.virli@hotmail.com>
2022-10-17 02:25:34 +0100

    app.js logic

---
+++ app.js
@@ -0,0 +1,25 @@
+//JS Logic for Jotter: To-Do Web App
+
+
+//Target element 'Button' by its ID 'add'
+add = document.getElementById("add");
+add.addEventListener('click', add_info);  //id.addEventListener('click', function(){})    Register an Event Listner, Call add_info() on click
+
+
+
+
+
+//Function to Append info to Local Storage and then update Table
+function add_info(){}
+
+
+
+
+//Function to Delete info from Local Storage and then update Table
+function erase(){}
+
+
+
+
+//Function to update Table
+function update_table(){}
